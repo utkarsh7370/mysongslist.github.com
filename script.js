5b397814543090f5f9627e9e356b164ac21edf94
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-02-27 19:48:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
+let progress = document.getElementById("progress");
+let total_duration = document.getElementById("duration");
+let total_currentTime = document.getElementById("current_time");
+const progress_div = document.getElementById("progress_div");
 
 const songs = [
     {
@@ -76,6 +80,45 @@ const prevSong = ()=>{
     loadSong(songs[songIndex]);
     playMusic();
 }
+
+music.addEventListener("timeupdate", (event)=>{
+    const { currentTime, duration} = event.srcElement;
+    let progress_time = (currentTime / duration) *100;
+    progress.style.width = `${progress_time}%`;
+
+    let min_duration = Math.floor(duration/60);
+    let sec_duration = Math.floor(duration % 60);
+
+    if(sec_duration <10){
+        sec_duration = `0${sec_duration}`
+    }
+
+    let tot_duration = `${min_duration}:${sec_duration}`;
+    if(duration){
+
+        total_duration.textContent = `${tot_duration}`;
+    }
+
+    let min_currentTime = Math.floor(currentTime/60);
+    let sec_currentTime = Math.floor(currentTime % 60);
+
+   
+    if(sec_currentTime <10){
+        sec_currentTime = `0${sec_currentTime}`
+    }
+    let tot_currentTime = `${min_currentTime}:${sec_currentTime}`;
+
+        total_currentTime.textContent = `${tot_currentTime}`;
+});
+
+music.addEventListener("ended", nextSong);
+
+progress_div.addEventListener("click", (event)=>{
+    const{duration} = music;
+    let move_progress = (event.offsetX/event.srcElement.clientWidth) *duration;
+    music.currentTime = move_progress;
+})
+
 
 next.addEventListener("click", nextSong);
 prev.addEventListener("click", prevSong);
